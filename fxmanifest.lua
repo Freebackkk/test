3e879b3a52c2dfb37af4f62778bb340f1923ae7f
FreeBack <freeback@netopeer.gg>
2025-09-15 17:56:31 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'FreeBack'
+description 'Test Resource for FiveM'
+version '1.0.0'
+
+-- Client scripts
+client_scripts {
+    'client/*.lua'
+}
+
+-- Server scripts
+server_scripts {
+    'server/*.lua'
+}
+
+-- Shared scripts
+shared_scripts {
+    'shared/*.lua'
+}
